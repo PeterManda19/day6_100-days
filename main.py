9c12da219444cbdfca1f3927cc53830a12846168
Peter Manda <109303643+PeterManda19@users.noreply.github.com>
2022-10-27 14:24:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print("SECURE LOGIN")
+username = input("Username > ")
+password = input("Password> ")
+
+if username == "mark" and password == "password":
+ print("Welcome Mark! Take charge and mark your territory!")
+elif username == "suzanne" and password == "Su74nne":
+ print("Hey there Suzanne! You look amazing!")
+elif username == "victor" and password == "Vi13tor":
+ print("Hey there Victor! Continue winning today!")  
+else:
+ print("You do not have access. Go away!")
